59047f81d095b11ddddb3c3a0a1bf4238ac2d3b8
Olami <truthin3Dimension@gmail.com>
2026-02-26 16:54:05 +0100

    updated export to expose parse

--- src/runtime/index.js
+++ src/runtime/index.js
@@ -1,4 +1,5 @@
 // src/runtime/index.js
 const { RuntimeAPI, execute } = require('./RuntimeAPI');
+const { parse} = require('../parser');
 
-module.exports = { execute, RuntimeAPI };
+module.exports = { execute, RuntimeAPI, parse };
